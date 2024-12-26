fd327d1e8ef69cda8a672d6c14175ab258ec6611
Duy4504 <nguyenvanduy176@gmail.com>
2024-12-26 17:14:10 +0700

    login register

--- newproject/newproject/Form2.Designer.cs
+++ newproject/newproject/Form2.Designer.cs
@@ -35,6 +35,8 @@
             textBox1 = new TextBox();
             textBox2 = new TextBox();
             textBox3 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
             // 
             // label1
@@ -42,63 +44,84 @@
             label1.AutoSize = true;
             label1.Location = new Point(59, 196);
             label1.Name = "label1";
-            label1.Size = new Size(50, 20);
+            label1.Size = new Size(132, 20);
             label1.TabIndex = 0;
-            label1.Text = "label1";
+            label1.Text = "Comfirm Password";
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Location = new Point(59, 130);
             label2.Name = "label2";
-            label2.Size = new Size(50, 20);
+            label2.Size = new Size(70, 20);
             label2.TabIndex = 1;
-            label2.Text = "label2";
+            label2.Text = "Password";
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Location = new Point(59, 70);
             label3.Name = "label3";
-            label3.Size = new Size(50, 20);
+            label3.Size = new Size(75, 20);
             label3.TabIndex = 2;
-            label3.Text = "label3";
+            label3.Text = "Username";
             // 
             // label4
             // 
             label4.AutoSize = true;
             label4.Location = new Point(249, 16);
             label4.Name = "label4";
-            label4.Size = new Size(50, 20);
+            label4.Size = new Size(72, 20);
             label4.TabIndex = 3;
-            label4.Text = "label4";
+            label4.Text = "ĐĂNG KÝ";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(174, 70);
+            textBox1.Location = new Point(215, 67);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(125, 27);
+            textBox1.Size = new Size(277, 27);
             textBox1.TabIndex = 4;
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(174, 130);
+            textBox2.Location = new Point(216, 127);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(125, 27);
+            textBox2.Size = new Size(276, 27);
             textBox2.TabIndex = 5;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(174, 189);
+            textBox3.Location = new Point(216, 193);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(125, 27);
+            textBox3.Size = new Size(276, 27);
             textBox3.TabIndex = 6;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(215, 259);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 7;
+            button1.Text = "Register";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(398, 259);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 8;
+            button2.Text = "Exit";
+            button2.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(577, 331);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(textBox3);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
@@ -121,5 +144,7 @@
         private TextBox textBox1;
         private TextBox textBox2;
         private TextBox textBox3;
+        private Button button1;
+        private Button button2;
     }
 }
--- newproject/newproject/Form2.cs
+++ newproject/newproject/Form2.cs
@@ -16,5 +16,10 @@ namespace newproject
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
