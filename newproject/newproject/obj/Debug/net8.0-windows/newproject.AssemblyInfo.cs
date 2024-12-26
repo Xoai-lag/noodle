6e58c6d27238781460ffe980b431279ae57c8d94
Xoai-lag <dh52200568@student.stu.edu.vn>
2024-12-26 17:15:36 +0700

    them admin

--- newproject/newproject/obj/Debug/net8.0-windows/newproject.AssemblyInfo.cs
+++ newproject/newproject/obj/Debug/net8.0-windows/newproject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("newproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08a0bfe8f0929bd52fce13f3ea44c0980d3c54f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("newproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("newproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
